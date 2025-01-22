918ceceb80992e83e5aa35ea775aa9da0b07ca82
Alvee <M.Alvee8141@gmail.com>
2025-01-23 00:28:32 +0600

    ts config updates (#2)

--- dist/index.js
+++ dist/index.js
@@ -25681,15 +25681,6 @@ var __importStar = (this && this.__importStar) || (function () {
         return result;
     };
 })();
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(7484));
 const os = __importStar(__nccwpck_require__(857));
@@ -25703,16 +25694,14 @@ function getAbsolutePath(inputFilePath) {
         return path.join(homeDirectory, inputFilePath.slice(1));
     throw new Error("Unable to resole `~` to HOME");
 }
-function run() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const path = getAbsolutePath(core.getInput("path", { required: true }));
-        core.debug("Path : " + path);
-        const text = core.getInput("trim") === 'true' ?
-            fs.readFileSync(path, { encoding: "utf8" }).trim() :
-            fs.readFileSync(path, { encoding: "utf8" });
-        core.debug("Text : " + text);
-        core.setOutput("text", text);
-    });
+async function run() {
+    const path = getAbsolutePath(core.getInput("path", { required: true }));
+    core.debug("Path : " + path);
+    const text = core.getInput("trim") === 'true' ?
+        fs.readFileSync(path, { encoding: "utf8" }).trim() :
+        fs.readFileSync(path, { encoding: "utf8" });
+    core.debug("Text : " + text);
+    core.setOutput("text", text);
 }
 run();
 exports["default"] = run;
